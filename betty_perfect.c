2c8c747869f4894d5f39e3587aa1939bd6992d78
mj-ad <adekunlemojolajah@gmail.com>
2022-11-14 11:36:03 +0100

    Betty would be proud

---
+++ betty_perfect.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *betty+perfect - a code that passes all on betty's checks
+ *@a: number to be multiplied
+ *
+ *Return: void
+ */
+
+void betty_perfect(int a)
+{
+int b;
+b = a * 2;
+printf("The product of %d and 2 is %d\n", a, b);
+}
