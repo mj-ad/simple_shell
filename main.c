810003b9c75c122216629126409b67c48362f149
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-09-21 00:11:32 +0100

    Tasks 1 to 3 completed

--- main.c
+++ main.c
@@ -1,67 +1,27 @@
 #include "shell.h"
 
-int main(int argc, char *argv[])
+/**
+ * main - Entry point
+ * @ac: counter
+ * @av: arguments
+ *
+ * Return: always 1 except in cases of an error
+ */
+
+void main(void)
 {
-    char *prompt = "#cisfun$ ";
-    char *line = NULL;
-    char *copy = NULL;
-    char *del = " \n";
-    size_t n = 0;
-    int read;
-    int ntokens = 0;
-    char *token;
-    int i;
+	char *line;
+	char **args;
+	int stats;
 
-    (void) argc;
+	do
+	{
+		printf("$ ");
+		line = read_line();
+		args = split_line(line);
+		stats = exec(args);
 
-    while (1)
-    {
-        printf("%s", prompt);
-        read = getline(&line, &n, stdin);
-
-        if (read == -1)
-        {
-            printf("Exiting shell....\n");
-            return (-1);
-        }
-
-        copy = malloc(sizeof(char) * read);
-        if (copy == NULL)
-        {
-            perror("Memory allocation error");
-            return (-1);
-        }
-
-        strcpy(copy, line);
-
-        token = strtok(line, del);
-
-        while(token != NULL)
-        {
-            ntokens++;
-            token = strtok(NULL, del);
-        }
-        ntokens++;
-
-        argv = malloc(sizeof(char *) * ntokens);
-
-        token = strtok(copy, del);
-
-        for (i = 0; token != NULL; i++)
-        {
-            argv[i] = malloc(sizeof(char) * strlen(token));
-            strcpy(argv[i], token);
-
-            token = strtok(NULL, del)
-        }
-        argv[i] = NULL;
-
-        exec(argv);
-
-    }
-
-    free(line);
-    free(copy);
-
-    return (0);
+		free(line);
+		free(args);
+	} while (stats);
 }
