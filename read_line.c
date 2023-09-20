aaeb126fb695d3be3f35a71b542900f3aaae0ebd
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-09-21 00:13:33 +0100

    Task 1 to 3 completed

---
+++ read_line.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * read_line - reads line from buffer
+ *
+ * Return: line read
+ */
+
+char *read_line(void)
+{
+	char *line = NULL;
+	ssize_t buf = 0;
+	ssize_t read;
+
+	if (read == getline(&line, &buf, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("Could not read line from stdin");
+			exit(EXIT_FAILURE);
+		}
+		free(line);
+	}
+	return (line);
+}
---
+++ split_line.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+/**
+ * split_line - splits the line gotten from get line into multiple strings
+ * @line: line to be split
+ * 
+ * Return: an array of split lines
+*/
+
+char **split_line(char *line)
+{
+	char **argv = NULL;
+	char *delim = ' ';
+	size_t word = 0;
+	size_t size = strlen(line);
+	int i = 0, j = 0;
+	char prev = '\0';
+
+	if (strtok(line, delim) != NULL)
+	{
+		++word;
+	}
+	while (strtok(NULL, delim))
+	{
+		++word;
+	}
+
+	argv = malloc(sizeof(char *) * (word + 1));
+	for (i = 0; i < size; ++i)
+	{
+		if (line[i] != '\0' && prev == '\0')
+		{
+			argv[j] = line + i, ++j;
+		}
+		prev = line[i];
+	}
+	argv[j] = NULL;
+	return (argv);
+}
